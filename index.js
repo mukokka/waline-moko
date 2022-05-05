1c82c856a039056918503edf20980ab88f8ab345
Muko <104675516+mukokka@users.noreply.github.com>
2022-05-05 17:35:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Waline = require('@waline/vercel');
 
 module.exports = Waline({
-  async preDelete(commentId) {
+  async preDelete(路人mukoko) {
     return "Then you can't delete comment";
   },
 });
