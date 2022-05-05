33ab81c42ffb30b806b1fb4013de1144ad9f5f17
Muko <104675516+mukokka@users.noreply.github.com>
2022-05-05 17:31:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Application = require('@waline/vercel');
+const Waline = require('@waline/vercel');
 
-module.exports = Application({
-  async postSave(comment) {
-    // do what ever you want after save comment
+module.exports = Waline({
+  async preDelete(commentId) {
+    return "Then you can't delete comment";
   },
 });
